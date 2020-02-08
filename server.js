5b09364e3a5f3108b3bcf95b1667112be3f9235e
Oretastic <hloreair@outlook.com>
2020-02-08 10:20:50 -0500

    did stuff

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 const db = require('./db');
 const path = require('path');
 
+app.delete('api/companies/:id', async (req, res, next) => {
+  try{
+    await db.destroy(req.params.id)
+    res.sendStatus(204)
+  } catch (ex){
+    next(ex)
+  }
+})
+
 app.get('/api/companies', (req, res, next)=> {
   db.readJSON('./companies.json')
     .then( companies => res.send(companies))
