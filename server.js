4fb233c99b8ba7bf32ed0219dfc790355d325cb0
Chaise Matev <aj_fl81@msn.com>
2020-02-08 11:05:23 -0500

    still phase one

--- server.js
+++ server.js
@@ -3,25 +3,29 @@ const app = express();
 const db = require('./db');
 const path = require('path');
 
-app.delete('api/companies/:id', async (req, res, next) => {
-  try{
-    await db.destroy(req.params.id)
-    res.sendStatus(204)
-  } catch (ex){
-    next(ex)
-  }
-})
+app.delete('/api/companies/:id', (req, res, next) => {
+  const id = req.params.id;
 
-app.get('/api/companies', (req, res, next)=> {
+  db.readJSON('./companies.json').then(items => {
+    return db
+      .writeJSON(
+        './companies.json',
+        items.filter(item => item.id !== id * 1)
+      )
+      .catch(next);
+  });
+});
+
+app.get('/api/companies', (req, res, next) => {
   db.readJSON('./companies.json')
-    .then( companies => res.send(companies))
+    .then(companies => res.send(companies))
     .catch(next);
 });
 
-app.get('/', (req, res, next)=> {
+app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
